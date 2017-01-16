fabb9cd1497e992f756c4b385354e973f7c57c21
Yurii Troniak <y.troniak@levi9.com>
2017-01-16 18:13:01 +0200

    init

---
+++ dist/script.js
@@ -0,0 +1,24 @@
+//let fraza = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde voluptatibus sunt magnam nemo, modi incidunt dolorum pariatur rem impedit asperiores iste mollitia tempora nisi, eveniet ullam ipsa! Possimus libero accusantium voluptatum aut, ullam laborum, delectus facilis deleniti qui asperiores et?";
+//class LetterCounter {
+//    constructor(text) {
+//        this.text = text;
+//        this.lettersCollection = {}
+//    }
+//    countLetters() {
+//        const letterArr = this.text.split('');
+//        letterArr.map((el, i) => {
+//            let letter_ = this.lettersCollection[el];
+//            return this.lettersCollection[el] = letterArr.filter(letter => letter === el).length;
+//        });
+//        return this.lettersCollection;
+//    }
+//}
+//let slovo = new LetterCounter();
+//let inputValue = document.querySelector('#input');
+//function keyPress(keyPresed) {
+//    if (keyPresed != " " && keyPresed.toString().length < 2) {
+//        slovo.countLetters(keyPresed);
+//        slovo.countLetters();
+//    }
+//}
+"use strict";
